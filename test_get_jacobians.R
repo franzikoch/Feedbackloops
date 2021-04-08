c8a2891a3f99b9e5259bee6490b676d97572aed5
Franzi Koch <franzi.koch@outlook.de>
2021-04-08 11:52:56 +0200

    updated paths+ assemble_jacobian_function

--- test_get_jacobians.R
+++ test_get_jacobians.R
@@ -1,6 +1,6 @@
 #testing get_jacobians_functions
-competition_path <- "~/Documents/UNI/Bryozoans_git/Rohdaten/Data_cleaned/Competition_Signy_1.csv"
-abundance_path <- "~/Documents/UNI/Bryozoans_git/Rohdaten/Data_cleaned/Abundance_Signy_1.csv"
+competition_path <- "~/Documents/Bryozoans/Rohdaten/Data_cleaned/Competition_Signy_1.csv"
+abundance_path <- "~/Documents/Bryozoans/Rohdaten/Data_cleaned/Abundance_Signy_1.csv"
 
 
 #format species contact matrix and abundance list
@@ -11,4 +11,4 @@ abundance <- read_data(competition_path, abundance_path)[[2]]
 interaction_table <- interaction_strengths(contact_matrix, abundance, c(-0.1, -0.9, -0.2))
 
 #assemble the Jacobian matrix
-Jacobian <- assemble_jacobian(interaction_table, abundance[,1])
+Jacobian <- assemble_jacobian(interaction_table, abundance[,1], "F_ij_B", "F_ji_B")
